f146762a59c290ebdef73b00932fb3ff92e87d85
Muhammadilhamakb <112915107+Muhammadilhamakb@users.noreply.github.com>
2022-09-07 14:43:00 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Nama:Muhammad Ilham Akbar" << endl;
+    cout << "Nim :A11.2022.14109" << endl;
+    cout << "Alamat:Desa Tamansari Rt 02 Rw III,Mranggen,Demak" << endl;
+    cout << "Asal Sekolah:MAN 1 KOTA SEMARANG" << endl;
+    return 0;
+}
